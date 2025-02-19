e8192c386b403438f4e239b3377e4f61b780ab22
sdpsd <sdpsd@ADMINISTRATOR>
2025-02-19 12:47:54 +0300

    Добавьте файлы проекта.

---
+++ Task_2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int t = c;
+c = b;
+b = a;
+a = t;
+Console.Write("{0} {1} {2}", a, b, c);
+Console.ReadKey();
